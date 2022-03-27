11ccff104ea07efb668506468a97c2ffe2d6391f
Damini Tiwari <94996554+DaminiTiwari@users.noreply.github.com>
2022-03-27 23:16:07 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import "firebase/compat/firestore"
 // import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQybE3IRAFZbiPokotCdfSdmma7_qsyRs",
-  authDomain: "clone-project-66aba.firebaseapp.com",
-  projectId: "clone-project-66aba",
-  storageBucket: "clone-project-66aba.appspot.com",
-  messagingSenderId: "474065775368",
-  appId: "1:474065775368:web:1e5006a6ef1e6377ae9abd"
+
 };
     
 const firebaseApp =  firebase.initializeApp(firebaseConfig);
